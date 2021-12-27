1065e24769839759b6ab270683653864af348f67
Kristaps Ritins <kritinsdev@gmail.com>
2021-12-28 01:19:22 +0200

    blog with mongodb finished

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ app.use(express.urlencoded({ extended: true })); // Parse incoming request bodie
 app.use(express.static('public')); // Serve static files (e.g. CSS files)
 
 app.use(blogRoutes);
+const db = require('./data/database');
 
 app.use(function (error, req, res, next) {
-  // Default error handling function
-  // Will become active whenever any route / middleware crashes
   console.log(error);
   res.status(500).render('500');
 });
 
-app.listen(3000);
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
+
+
